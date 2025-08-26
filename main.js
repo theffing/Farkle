37308d7713d9e6d33ea1dd874d26c55d7ba4fd48
Taylor <heffingtontaylor@gmail.com>
2025-08-25 20:14:38 -0700

    Added a new button, Pass, and added comments to main.js to begin solidifying rules

---
+++ main.js
@@ -0,0 +1,11 @@
+// When the user starts the Farkle the bank and hand is set to 0 and all die are set to the 1 position
+// click roll to begin
+//      roll all six die
+//      
+//      if any die is clicked
+//          mark it
+//      if 1 or more die are clicked and hold is clicked
+//          add appropriate score from clicked die to hand
+//      if pass is clicked
+//          add hand to bank
+// 
